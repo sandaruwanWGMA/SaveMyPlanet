7132507d016b0f8339f6695d7ec8c950269417de
SandaruwanWGMA <molinduachintha24@gmail.com>
2024-05-04 19:52:44 +0530

    Initial Commit

---
+++ SaveMyPlanet/SaveMyPlanetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveMyPlanetApp.swift
+//  SaveMyPlanet
+//
+//  Created by Molindu Achintha on 2024-05-04.
+//
+
+import SwiftUI
+
+@main
+struct SaveMyPlanetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
